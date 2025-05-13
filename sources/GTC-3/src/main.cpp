ec59631f78684223ce8ce8f6297527941eeb7f88
Sasha7b9Work <Sasha7b9@tut.by>
2025-05-13 12:02:29 +0300

    Не компилируется компилятором не 6.23

--- sources/GTC-3/src/main.cpp
+++ sources/GTC-3/src/main.cpp
@@ -1,16 +1,25 @@
 // (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Device.h"
+#include <cstdio>
 
 /*
 *   Каждое изделие обладает уникальным ID, который вводится после прошивки через меню
 */
 
 
+#ifndef WIN32
+    #if __ARMCC_VERSION != 6230001
+        // На других версиях компиляторов не проверялось
+        #error "Requires ARM Compiler V6.23 from uVision 5.42a"
+    #endif
+#endif
+
+
 int main(void)
 {
     Device::Init();
-
+    
     while (true)
     {
         Device::Update();
