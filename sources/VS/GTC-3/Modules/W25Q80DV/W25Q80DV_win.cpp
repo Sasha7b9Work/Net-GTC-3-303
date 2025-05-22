d5bfb89516e489c043e0ceb021df26018b986efd
Sasha7b9Work <Sasha7b9@tut.by>
2025-05-22 17:02:31 +0300

    *

---
+++ sources/VS/GTC-3/Modules/W25Q80DV/W25Q80DV_win.cpp
@@ -0,0 +1,11 @@
+// 2025/05/22 17:01:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Modules/W25Q80DV/W25Q80DV.h"
+
+
+bool W25Q80DV::Test::Run()
+{
+    LOG_ERROR("Not implemented");
+
+    return false;
+}
